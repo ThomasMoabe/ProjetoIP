767a8f56a243ce6a287896374fead58ca2020ba3
Thomas <thomasanderson@outlook.com.br>
2015-06-21 16:25:58 -0300

    Melhorias relacionadas à pesquisar por hora e data no BD e ordenação de resultados acrescentada

--- src/lan/bd/Tabela.java
+++ src/lan/bd/Tabela.java
@@ -40,32 +40,35 @@ public abstract class Tabela { //um repositório genérico que é tratado da mesma 
 	public int getIdAtual() {
 		return this.idatual;
 	}
-	/*pesquisas em tabelas seguem a seguinte formatação de filtragem {campo:valor}, pode ser ultilizado mais de um filtro separando por espaço, a única condição suportada é a iguadade*/
-	/*public Registro[] procura(String parametros) {
-		String[] parametrosarray = this.ExplodeParametros(parametros);
-		RepositorioLista encontrados = new RepositorioLista("encontrados", this.campostipos);
-		int indice = 0;
-		Registro atual;
-		while ((atual = this.getRegistro(indice, 0, this)) != null) { //varre todos os registros de forma genérica na tabela, independente do seu tipo
-			boolean insere = true;
-			String[] atuais = this.ExplodeParametros(atual.toString());
-			for (int i = 0; i < parametrosarray.length && insere == true; i++) { //enquanto não encontrar um parâmetro que não satisfaça..
-				if (atual.toString().indexOf(parametrosarray[i]) < 0) {
-					insere = false;
-				}
-			}
-			if (insere) {
-				encontrados.inserir(atual);
-			}
-			insere = true;
-			indice++;
-		}
-		return encontrados.toArray();
-	}*/
 	
 	private Registro[] procuraInterno(String parametros, boolean ignorecase) {
 		parametros = parametros.replaceAll("}\\s+","}");
 		parametros = parametros.replaceAll("\\s+\\{","{");
+		
+		//Cláusula ORDER BY	
+		boolean ordena = false;
+		String[] listacamposordena = new String[1];
+		boolean ordemcrescente = true;
+		{
+			if (parametros.matches(".*}\\s*(?i)order\\s+by{1}.*")) {
+				ordena = true;
+				String[] quebraordem = parametros.split("(?i)order\\s+by{1}\\s+");
+				if (quebraordem.length > 1) {
+					parametros = quebraordem[0];
+					String camposordena;
+					if (quebraordem[1].length() > (camposordena = quebraordem[1].replaceAll("\\s+(?i)desc$", "")).length()) {
+						ordemcrescente = false;
+					} else {
+						camposordena =  quebraordem[1].replaceAll("\\s+(?i)asc$", "");
+					}
+					if (camposordena.indexOf(",") < 0) { //só tem um campo para ordenação
+						listacamposordena[0] = camposordena;
+					} else {
+						listacamposordena = camposordena.split("\\s*,\\s*");
+					}
+				}
+			}
+		}//fim do tratamento ORDER BY
 		parametros = parametros.replaceAll("}OR\\{","}or{"); 
 		String[] conjuntoparametros = new String[1];
 		if (parametros.indexOf("}or{") > 0) {
@@ -107,7 +110,7 @@ public abstract class Tabela { //um repositório genérico que é tratado da mesma 
 			}
 			indice++;
 		}
-		return encontrados.toArray();
+		return ordena ? this.ordena(encontrados.toArray(), listacamposordena, ordemcrescente) : encontrados.toArray();
 	}
 	
 	public Registro[] procura(String parametros) {
@@ -118,9 +121,30 @@ public abstract class Tabela { //um repositório genérico que é tratado da mesma 
 		return this.procuraInterno(parametros, true);
 	}
 	
+	private Registro[] ordena(Registro[] registros, String[] campos, boolean crescente) {
+		for (int i = campos.length-1; i >= 0; i--) { //ordena por todos os campos começando pelo ultimo que tem menos prioridade
+			for (int j = 0; j < registros.length; j++) { //testa todos os registros
+				for (int k = 0; k < registros.length-1; k++) { //em todas as posições
+					int posicaocampo = this.getPosicaoCampo(campos[i]);
+					String valorregistro = this.ExplodeParametros(registros[k].toString())[posicaocampo];
+					String valorcompara = this.ExplodeParametros(registros[k+1].toString())[posicaocampo];
+					valorcompara = valorcompara.replaceAll("^\\{" + campos[i] + "=", "{" + campos[i] + (crescente ? ">" : "<"));
+					boolean muda = this.compara(valorregistro, valorcompara, true);
+					if (muda) {
+						Registro temp = registros[k];
+						registros[k] = registros[k+1];
+						registros[k+1] = temp; 
+					}
+				}
+			}
+		}
+		System.out.println("ordenando");
+		return registros;
+	}
+	
 	private boolean compara(String atual, String condicao, boolean ignorecase) {
 		boolean insere = false;
-		String valoratual;
+		String valoratual = "";
 		String campo;
 		int posicaocampo = -1;
 		String tipo;
@@ -130,7 +154,9 @@ public abstract class Tabela { //um repositório genérico que é tratado da mesma 
 		atual = atual.substring(1, atual.length()-1);
 		String[] atualexplode = atual.split("=");
 		campo = atualexplode[0];
-		valoratual = atualexplode[1];
+		for (int i = 1; i < atualexplode.length; i++) { //resolvido problema de operator = na string, só o primeiro é separador, os outros fazem parte da string
+			valoratual += atualexplode[i];
+		}
 		posicaocampo = this.getPosicaoCampo(campo);
 		tipo = this.tipos[posicaocampo];
 		
@@ -148,11 +174,36 @@ public abstract class Tabela { //um repositório genérico que é tratado da mesma 
 		condicaoexplode = condicao.split(separador);
 		condicaovalor = condicaoexplode[1];
 		
+		boolean inverte;
+		
 		switch (tipo) {
 		case "string":
-			String valoratualstring = ignorecase ? valoratual.toLowerCase() : valoratual;
-			String valorcomparastring = ignorecase ? condicaovalor.toLowerCase() : condicaovalor;
-			insere = valoratualstring.matches(valorcomparastring);
+			if ((condicao.indexOf(campo + "=") > -1) && (condicao.indexOf("=") == campo.length())) { //na string é preciso ter mais cuidado, pois os operadores podem estar na própria palavra, dessa forma fica garantida a posição do sinal
+				String valoratualstring = ignorecase ? valoratual.toLowerCase() : valoratual;
+				String valorcomparastring = ignorecase ? condicaovalor.toLowerCase() : condicaovalor;
+				insere = valoratualstring.matches(valorcomparastring);
+			} else if (((condicao.indexOf(campo + ">") > -1) && (condicao.indexOf(">") == campo.length()) || (condicao.indexOf(campo + ">=") > -1) && (condicao.indexOf(">=") == campo.length())) & !(inverte = false) ||
+						((condicao.indexOf(campo + "<") > -1) && (condicao.indexOf("<") == campo.length()) || (condicao.indexOf(campo + "<=") > -1) && (condicao.indexOf("<=") == campo.length())) & (inverte = true)) {
+				String stringmaior;
+				String stringmenor;
+				if (valoratual.length() >= condicaovalor.length()) {
+					stringmaior = valoratual;
+					stringmenor = condicaovalor;
+				} else {
+					stringmaior = condicaovalor;
+					stringmenor = valoratual;
+					inverte = !inverte;
+				}
+				boolean definicao = false;
+				for (int i = 0; i < stringmenor.length() && definicao == false; i++) {
+					if (stringmenor.toLowerCase().charAt(i) != stringmaior.toLowerCase().charAt(i)) {
+						definicao = true; System.out.println(inverte);
+						insere = stringmenor.toLowerCase().charAt(i) < stringmaior.toLowerCase().charAt(i) ? (!inverte) : (inverte);
+					} else {
+						insere = true;
+					}
+				}
+			}
 			break;
 		case "int":
 			int valoratualinteiro = Integer.parseInt(valoratual);
@@ -178,10 +229,20 @@ public abstract class Tabela { //um repositório genérico que é tratado da mesma 
 			break;
 		case "data":
 			String[] separadataatual = valoratual.split("/");
-			String dataatualcompleta = separadataatual[2] + separadataatual[1] + separadataatual[0]; //de trás pra frente, pois o ano tem mais importancia que o mês e o mês que o dia
+			String dataatualcompleta = "";//separadataatual[2] + separadataatual[1] + separadataatual[0]; //de trás pra frente, pois o ano tem mais importancia que o mês e o mês que o dia
+			String datacomparacompleta = "";
+			if (condicaovalor.indexOf("/") > 0) {
+				String[] separadatacompara = condicaovalor.split("/");
+				for (int i = separadatacompara.length-1; i >= 0; i--) {
+					datacomparacompleta += separadatacompara[i];
+					dataatualcompleta += separadataatual[i];
+				}
+			} else {
+				datacomparacompleta += condicaovalor;
+				dataatualcompleta += separadataatual[0];
+			}
+			//String datacomparacompleta = separadatacompara[2] + separadatacompara[1] + separadatacompara[0];
 			int dataatualinteiro = Integer.parseInt(dataatualcompleta);
-			String[] separadatacompara = condicaovalor.split("/");
-			String datacomparacompleta = separadatacompara[2] + separadatacompara[1] + separadatacompara[0];
 			int datacomparainteiro = Integer.parseInt(datacomparacompleta);
 			if ((separador.equals("=") && dataatualinteiro == datacomparainteiro) || 
 					(separador.equals(">") &&  dataatualinteiro > datacomparainteiro) ||
@@ -193,10 +254,20 @@ public abstract class Tabela { //um repositório genérico que é tratado da mesma 
 			break;
 		case "hora":
 			String[] separahoraatual = valoratual.split(":");
-			String horaatualcompleta = separahoraatual[0] + separahoraatual[1] + separahoraatual[2];
+			String horaatualcompleta = "";//separahoraatual[0] + separahoraatual[1] + separahoraatual[2];
+			String horacomparacompleta = "";
+			if (condicaovalor.indexOf(":") > 0) {
+				String[] separahoracompara = condicaovalor.split(":");
+				for (int i = 0; i < separahoracompara.length; i++) {
+					horacomparacompleta += separahoracompara[i];
+					horaatualcompleta += separahoraatual[i];
+				}
+			} else {
+				horaatualcompleta = separahoraatual[0];
+				horacomparacompleta = condicaovalor;
+			}
+			
 			int horaatualinteiro = Integer.parseInt(horaatualcompleta);
-			String[] separahoracompara = condicaovalor.split(":");
-			String horacomparacompleta = separahoracompara[0] + separahoracompara[1] + separahoracompara[2];
 			int horacomparainteiro = Integer.parseInt(horacomparacompleta);
 			if ((separador.equals("=") && horaatualinteiro == horacomparainteiro) || 
 					(separador.equals(">") &&  horaatualinteiro > horacomparainteiro) ||
