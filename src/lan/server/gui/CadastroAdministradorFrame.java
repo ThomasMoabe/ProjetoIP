a6269d23fc80c711cdf2ac4be47f9db6fe6c585d
Thomas <thomasandersonscc@gmail.com>
2015-06-30 16:44:20 -0300

    janela inicial de cadastro de administrador

---
+++ src/lan/server/gui/CadastroAdministradorFrame.java
@@ -0,0 +1,128 @@
+package lan.server.gui;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import lan.server.administracao.AdministradorJaCadastradoException;
+import lan.server.administracao.AdministradorLoginInvalidoException;
+import lan.server.administracao.AdministradorSenhaFracaException;
+import lan.server.painel.Lan;
+
+public class CadastroAdministradorFrame extends JFrame {
+	private Lan lan;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JPasswordField passwordField;
+	private JLabel lblLogin;
+	private JLabel lblNome;
+	private JLabel lblSenha;
+	private JPasswordField passwordField_1;
+
+	public CadastroAdministradorFrame(Lan lan) {
+		this.lan = lan;
+		setResizable(false);
+		setTitle("Cadastro de administrador");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 350, 412);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCadastroDeAdministrador = new JLabel("Cadastro de administrador");
+		lblCadastroDeAdministrador.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCadastroDeAdministrador.setFont(new Font("Stencil", Font.PLAIN, 14));
+		lblCadastroDeAdministrador.setBounds(29, 28, 282, 14);
+		contentPane.add(lblCadastroDeAdministrador);
+		
+		JLabel lblCaroUsurioPara = new JLabel("<html>Caro usu\u00E1rio, para utiliza\u00E7\u00E3o deste software \u00E9 necess\u00E1rio o cadastro de um administrador, por favor preencha os campos abaixo:</html>");
+		lblCaroUsurioPara.setBounds(29, 67, 282, 45);
+		contentPane.add(lblCaroUsurioPara);
+		
+		textField = new JTextField();
+		textField.setBounds(160, 177, 151, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(160, 208, 151, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(160, 239, 151, 20);
+		contentPane.add(passwordField);
+		
+		lblLogin = new JLabel("Login:");
+		lblLogin.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblLogin.setBounds(104, 211, 41, 14);
+		contentPane.add(lblLogin);
+		
+		lblNome = new JLabel("Nome:");
+		lblNome.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNome.setBounds(104, 180, 41, 14);
+		contentPane.add(lblNome);
+		
+		lblSenha = new JLabel("Senha:");
+		lblSenha.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSenha.setBounds(104, 242, 41, 14);
+		contentPane.add(lblSenha);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(29, 144, 282, 2);
+		contentPane.add(separator);
+		
+		passwordField_1 = new JPasswordField();
+		passwordField_1.setBounds(160, 271, 151, 20);
+		contentPane.add(passwordField_1);
+		
+		JLabel lblConfirmaoDeSenha = new JLabel("Confirme a senha:");
+		lblConfirmaoDeSenha.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblConfirmaoDeSenha.setBounds(28, 274, 117, 14);
+		contentPane.add(lblConfirmaoDeSenha);
+		
+		JButton btnContinuar = new JButton("Continuar");
+		btnContinuar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if (passwordField.getText().equals(passwordField_1.getText())) {
+					cadastra(textField_1.getText() , textField.getText() ,passwordField.getText());
+				} else {
+					new JOptionPane().showMessageDialog(null, "Você deve inserir a mesma senha nos dois campos", "Senha não confere", JOptionPane.WARNING_MESSAGE);
+				}
+			} 
+		});
+		btnContinuar.setBounds(124, 327, 109, 33);
+		contentPane.add(btnContinuar);
+		
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+	public void cadastra(String login, String nome, String senha) {
+		try {
+			this.lan.cadastraAdministrador(login, nome, senha);
+			JOptionPane.showMessageDialog(null, "Administrador cadastrado com sucesso!");
+			this.dispose();
+			new LoginFrame(this.lan);
+		} catch (AdministradorLoginInvalidoException e) {
+			new JOptionPane().showMessageDialog(null, e.getMessage(), "Login inválido", JOptionPane.WARNING_MESSAGE);
+		} catch (AdministradorSenhaFracaException e) {
+			new JOptionPane().showMessageDialog(null, e.getMessage(), "Senha fraca", JOptionPane.WARNING_MESSAGE);
+		} catch (AdministradorJaCadastradoException e) {
+			new JOptionPane().showMessageDialog(null, e.getMessage(), "Administrador já cadastrado", JOptionPane.WARNING_MESSAGE);
+		}
+	}
+}
--- src/lan/server/gui/JLan.java
+++ src/lan/server/gui/JLan.java
@@ -10,6 +10,8 @@ import javax.swing.border.EmptyBorder;
 
 import lan.server.painel.Lan;
 import lan.server.util.ConfiguracaoInvalidaException;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class JLan extends JFrame {
 	private Lan lan;
@@ -27,6 +29,12 @@ public class JLan extends JFrame {
 					frame = new JLan();
 					frame.setLocationRelativeTo(null); //centraliza na tela
 					frame.setVisible(true);
+					frame.dispose();
+					if (frame.getLan().liberalogin()) {
+						new LoginFrame(frame.getLan());
+					} else {
+						new CadastroAdministradorFrame(frame.getLan());
+					}
 				} catch (ConfiguracaoInvalidaException e) {
 					new JOptionPane().showMessageDialog(frame, e.getMessage(), "Erro de configuração",JOptionPane.ERROR_MESSAGE);
 				} catch (Exception e) {
@@ -41,14 +49,24 @@ public class JLan extends JFrame {
 	 * @throws ConfiguracaoInvalidaException 
 	 */
 	public JLan() throws ConfiguracaoInvalidaException {
+		setTitle("JLan - Carregando");
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 174);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblCarregandoArquivosDe = new JLabel("Carregando arquivos de configura\u00E7\u00E3o");
+		lblCarregandoArquivosDe.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCarregandoArquivosDe.setBounds(10, 120, 424, 14);
+		contentPane.add(lblCarregandoArquivosDe);
 		
 		this.lan = new Lan();
 	}
-
+	
+	public Lan getLan() {
+		return this.lan;
+	}
 }
